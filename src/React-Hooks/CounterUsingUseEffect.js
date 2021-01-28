da4c8b64a45c7d74ada1dbf0494be7dfe31896d7
Bitan Das <bitandas96@gmail.com>
2021-01-28 12:19:48 +0530

    React Hooks: Example of useEffect & update it while only count value change

--- src/React-Hooks/CounterUsingUseEffect.js
+++ src/React-Hooks/CounterUsingUseEffect.js
@@ -3,13 +3,16 @@ import React, { useState, useEffect } from 'react'
 function CounterUsingUseEffect() {
 
     const [count, setCount] = useState(0)
-    
+    const [name, setName] = useState('')
+
     useEffect(() => {
+        console.log("Use Effect- Updating the component")
         document.title = `You Clicked ${count} times`
-    })
+    }, [count])
 
     return (
         <div>
+            <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
             <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>Click {count} times</button>
         </div>
     )
